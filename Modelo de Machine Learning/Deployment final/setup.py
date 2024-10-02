4f7d53e87527f0f588f1493ade351046290fa0c3
Felipe Orlando Amezquita Samudio <143222330+psm545@users.noreply.github.com>
2024-10-01 19:48:30 -0500

    Delete Modelo de Machine Learning directory

--- Modelo de Machine Learning/Deployment final/setup.py
+++
@@ -1,9 +0,0 @@
-import spacy
-import os
-
-# Verificar si el modelo ya está descargado
-try:
-    spacy.load("en_core_web_sm")
-except OSError:
-    # Descargar el modelo si no está disponible
-    os.system("python -m spacy download en_core_web_sm")
